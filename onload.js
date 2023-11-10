40bea74f678f3eaf077ff423d15e4a6f46ac9c6d
Joar Heimonen <joarheimonen@live.no>
2023-11-10 19:29:28 +0100

    added print

--- onload.js
+++ onload.js
@@ -3,4 +3,13 @@
 window.onload = function() {
     loadTimeline();
     loadBackground();
+
+    //printing stuff
+    window.old_print = window.print;
+    window.print = function() {
+        document.getElementById("github").innerHTML = "github.com/Slenderman00";
+
+        window.old_print();
+        window.location.reload();
+    }
 }
